66c01abad277aa2b713cebf846c0f53b080b8541
Chiffilin <dimaaitbaev42@gmail.com>
2020-03-08 08:45:48 +0200

    Добавьте файлы проекта.

---
+++ Lab1.1C/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab1Sh
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Lab1.Task1");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a + b);
+
+        }
+    }
+}
